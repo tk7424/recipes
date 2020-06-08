68127f7afa524a7b4ffa07ea17995cb353073c0d
EC2 Default User <ec2-user@ip-172-31-20-241.us-east-2.compute.internal>
2020-06-08 14:43:07 +0000

    APP remake

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
+  
+  root to: "home#index"
 
   devise_for :users, :controllers => {
-    registrations: 'users/registrations',
     sessions: 'users/sessions'   
   } 
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
   
-  root to: "home#index"
+  
   resources :users
   resources :recipes
   
